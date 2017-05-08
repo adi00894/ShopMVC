cea2cf57694e7fb3646046ff18846f3f3109b2f7
Adrian Koszyk <adriankoszyk94@gmail.com>
2017-05-08 12:01:07 +0200

    Dodanie widokow rejestracji i logowania

---
+++ PraktyczneKursy/PraktyczneKursy/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using PraktyczneKursy.ViewModels;
+
+namespace PraktyczneKursy.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            //  logger.Info("Logowanie start");
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+
+
+            }
+
+        }
+        public ActionResult Register( )
+        {
+            //  logger.Info("Logowanie start");
+
+          //  ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+
+
+            }
+
+        }
+
+    }
+}
